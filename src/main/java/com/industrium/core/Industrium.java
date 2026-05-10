b90dd49c656794727ed93981f8ee052ab58d6740
deagle314 <deserteagle3141@gmail.com>
2026-05-10 14:30:44 +0900

    Update Industrium.java

--- src/main/java/com/industrium/core/Industrium.java
+++ src/main/java/com/industrium/core/Industrium.java
@@ -4,8 +4,10 @@ import com.industrium.core.common.registry.ModRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("industrium")
+@Mod(Industrium.MOD_ID)
 public class Industrium {
+
+    public static final String MOD_ID = "industrium";
 
     public Industrium() {
         ModRegistry.register(
