51bc3a16a02bf3f0519e70bfd890374c0be809a2
deagle314 <deserteagle3141@gmail.com>
2026-05-10 15:58:30 +0900

    Update PowerCableBlock.java

--- src/main/java/com/industrium/core/common/power/block/PowerCableBlock.java
+++ src/main/java/com/industrium/core/common/power/block/PowerCableBlock.java
@@ -1,39 +1,14 @@
-package com.industrium.core.common.power.block;
+package com.industrium.core.common.block;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.Level;
 
-/**
- * LV Power Cable - basic power transmission.
- * Connects machines and transfers power at LV tier.
- */
+/** Power Cable - transfers energy between blocks. */
 public class PowerCableBlock extends Block {
-    
     public PowerCableBlock() {
         super(Block.Properties.of()
             .strength(1.0f, 5.0f)
             .sound(SoundType.METAL)
             .noOcclusion());
-    }
-    
-    
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new CableBlockEntity(pos, state);
-    }
-
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
-        return (l, pos, st, te) -> {
-            if (te instanceof CableBlockEntity cable) {
-                cable.tickServer();
-            }
-        };
     }
 }
