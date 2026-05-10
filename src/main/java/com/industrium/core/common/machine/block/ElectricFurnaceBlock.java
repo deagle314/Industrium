722ce5b151d7f9f7b9852950fbcb4409a599a21d
deagle314 <deserteagle3141@gmail.com>
2026-05-10 15:51:24 +0900

    Update ElectricFurnaceBlock.java

--- src/main/java/com/industrium/core/common/machine/block/ElectricFurnaceBlock.java
+++ src/main/java/com/industrium/core/common/machine/block/ElectricFurnaceBlock.java
@@ -1,25 +1,30 @@
 package com.industrium.core.common.machine.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.Level;
 
 /**
  * Electric Furnace - powered smelting machine.
  * Uses LV power to smelt items automatically.
  */
-public class ElectricFurnaceBlock extends Block {
+public class ElectricFurnaceBlock extends BaseEntityBlock {
 
     public ElectricFurnaceBlock() {
-        super(Block.Properties.of()
+        super(Properties.of()
             .strength(2.0f, 10.0f)
             .sound(SoundType.METAL));
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Override
@@ -28,7 +33,9 @@ public class ElectricFurnaceBlock extends Block {
     }
 
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+            Level level, BlockState state, BlockEntityType<T> type) {
+
         return (l, pos, st, te) -> {
             if (te instanceof ElectricFurnaceBlockEntity furnace) {
                 furnace.tickServer();
